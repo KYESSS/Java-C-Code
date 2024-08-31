ab676783f0c6db3eea3b8ccd75f7c1bffd99feda
KYESSS <113056636+KYESSS@users.noreply.github.com>
2024-08-31 13:11:08 +0100

    Add files via upload

---
+++ java.tst7 Rock Paper Scissors Game.cpp
@@ -0,0 +1,46 @@
+#include <iostream> 
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    string player;
+    string opponent;
+    int playerScore = 0;
+    int opponentScore = 0;
+    string rps[] = {"rock", "paper", "scissors"};
+
+//srand = random seed value based on time
+srand((unsigned int) time(NULL)); // time_t, cast to unsigned int
+
+    while (true) {
+        cout << "Enter rock, paper, or scissors:";
+        getline(cin, player);
+
+         if (player != "rock" && player != "paper" && player != "scissors"){
+            break;
+         }  
+
+         opponent = rps[rand() % 3]; // rand() generates random number, then do modulus 3
+          // remainder after dividing 3 can be from 0 to 2, so we use this as the index number
+          cout << "Opponent chooses " << opponent << endl;
+
+          if (player == opponent) {
+            playerScore++;
+            opponentScore++;
+          }
+          else if (player == "rock") { 
+            if (opponent == "scissors") playerScore++;
+            else if (opponent == "paper") opponentScore++;
+             }
+             else if (player == "paper") {
+                if (opponent == "rock") playerScore++;
+                else if (opponent == "scissors") opponentScore++; 
+             }
+                else if (player == "scissors") {
+                    if (opponent == "paper") playerScore++;
+                    else if (opponent == "rock") opponentScore++; 
+    }
+    cout << "Player: " << playerScore << " Opponent: " << opponentScore << endl;
+}
+}
