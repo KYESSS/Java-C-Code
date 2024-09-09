d397fd51df4efdef43dc014bb1f976f5d222ba58
KYESSS <113056636+KYESSS@users.noreply.github.com>
2024-09-09 18:31:15 +0100

    Add files via upload

---
+++ java.tst18.1 Const Reference.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void printVector(const vector<string>& vec) { //cities =
+     for (const string& item : vec) {
+        cout << item << " ";
+     }
+     cout << endl;
+}
+
+
+int main() {
+ // Pass by value - generally used for primitive type (non class) ex) int, double, float, bool, char
+ // Pass by reference - generally used for class types ex) string, vector
+ // Pass by Const Reference - same as above except const, only for reading the parameter and not modifying
+
+ vector<string> cities = {"Newcastle", "Sunderland", "London"};
+ vector<string> fruits = {"Apple", "Banana", "Lemon", "Orange", "Strawberry"};
+ printVector(cities);
+ printVector(fruits);
+}
+
